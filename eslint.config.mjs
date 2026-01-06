9fe0f4d60d353e38e78f7734d09ed67c6d6f4eab
Jaden Scali <jadenscali132@gmail.com>
2026-01-06 09:17:24 -0500

    fix: prettier setup with husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
