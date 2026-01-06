c9351138bea202ca781d0015fc087c840e975d05
Jaden Scali <jadenscali132@gmail.com>
2026-01-06 09:53:20 -0500

    dev: precommit now only typechecks and lints staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Ask easy with AskEasy
+            Hello, ASK easy with AskEasy.
           </h1>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
