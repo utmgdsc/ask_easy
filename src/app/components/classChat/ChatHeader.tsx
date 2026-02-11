764c5514282cc30065a09c4f2db30b59316a888e
Manjyot Singh <manjyotsinghbirdi@gmail.com>
2026-02-11 13:43:54 -0500

    feat: slide hide/unhide capabilities added. Current slide width is restored when the slides are opened and closed again

--- src/app/components/classChat/ChatHeader.tsx
+++ src/app/components/classChat/ChatHeader.tsx
@@ -1,12 +1,52 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
+import { useContext } from "react";
+import { SlideUpdateContext } from "../room";
+import { PanelRightClose } from "lucide-react";
+import { useMediaQuery } from "@/hooks/use-media-query";
+
+function SlideToggle() {
+  // toggles slides on and off
+  const { isHidden, rerender } = useContext(SlideUpdateContext);
+  const isMDsize = useMediaQuery("(min-width: 1024px)");
+  if (!isMDsize) {
+    return (
+      <button
+        className="w-10 h-10 flex items-center justify-center text-stone-400 hover:text-stone-700 hover:bg-stone-200 rounded-md transition-colors"
+        onClick={() => rerender()}
+      >
+        {isHidden ? (
+          <PanelRightClose className="w-6 h-6 rotate-270" />
+        ) : (
+          <PanelRightClose className="w-6 h-6 rotate-90" />
+        )}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className="w-10 h-10 flex items-center justify-center text-stone-400 hover:text-stone-700 hover:bg-stone-200 rounded-md transition-colors"
+        onClick={() => rerender()}
+      >
+        {isHidden ? (
+          <PanelRightClose className="w-6 h-6 rotate-180" />
+        ) : (
+          <PanelRightClose className="w-6 h-6 " />
+        )}
+      </button>
+    );
+  }
+}
 
 export default function ChatHeader() {
   return (
     <div className="flex flex-col border-b bg-background sticky bg-stone-50 top-0 z-10">
       <header className="px-4 py-2 flex items-center justify-between">
-        <h1 className="text-xl font-bold">CSC209</h1>
+        <div className="flex items-center justify-left gap-2 ">
+          <SlideToggle />
+          <h1 className="text-xl font-bold">CSC209</h1>
+        </div>
         <Input className="h-10 w-1/2 max-w-xs bg-stone-200" placeholder="Search" />
       </header>
     </div>
--- src/app/components/room.tsx
+++ src/app/components/room.tsx
@@ -2,21 +2,63 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/componen
 import ClassChat from "./classChat";
 import SlideViewer from "./slideViewer";
 import { useMediaQuery } from "@/hooks/use-media-query";
+import { createContext, useState } from "react";
 
-export default function Room() {
-  const isMdSize = useMediaQuery("(min-width: 1024px)");
+export const SlideUpdateContext = createContext({ isHidden: false, rerender: () => {} });
 
+function chatPanel(isMdSize: boolean) {
   return (
     <div className="h-screen w-full bg-background font-sans">
       <ResizablePanelGroup direction={isMdSize ? "horizontal" : "vertical"}>
-        <ResizablePanel defaultSize={75} minSize={0}>
-          <SlideViewer />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={30} minSize={30}>
+        <ResizablePanel minSize={30}>
           <ClassChat />
         </ResizablePanel>
       </ResizablePanelGroup>
+    </div>
+  );
+}
+
+function chatAndSlidePanel(
+  isMdSize: boolean,
+  resizableWidth: number,
+  setResizableWidth: (width: number) => void
+) {
+  return (
+    <div className="h-screen w-full bg-background font-sans">
+      <ResizablePanelGroup direction={isMdSize ? "horizontal" : "vertical"}>
+        <ResizablePanel
+          defaultSize={100 - resizableWidth}
+          minSize={0}
+          onResize={(panelWidth) => {
+            setResizableWidth(panelWidth);
+          }}
+        >
+          <SlideViewer />
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={resizableWidth} minSize={30}>
+          <ClassChat />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+}
+
+export default function Room() {
+  const isMdSize = useMediaQuery("(min-width: 1024px)");
+  const [isHidden, setIsHidden] = useState(false);
+  const [resizableWidth, setResizableWidth] = useState(30);
+
+  function rerender() {
+    setIsHidden((prev) => !prev);
+  }
+  return (
+    <div className="h-screen w-full bg-background font-sans">
+      <SlideUpdateContext.Provider value={{ isHidden, rerender }}>
+        {isHidden
+          ? chatAndSlidePanel(isMdSize, resizableWidth, setResizableWidth)
+          : chatPanel(isMdSize)}
+      </SlideUpdateContext.Provider>
     </div>
   );
 }
