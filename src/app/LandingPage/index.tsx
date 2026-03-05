5bb3d8ffcff23a1810796c9ba93f1b82005cb005
bird <manjyotsinghbirdi@gmail.com>
2026-03-04 22:23:06 -0500

    feat: added a footer to the landing page

--- src/app/LandingPage/index.tsx
+++ src/app/LandingPage/index.tsx
@@ -13,27 +13,43 @@ function renderCourseButtons() {
     }
   }
   return (
-    <div className="flex-1 p-4 py-10  w-full ">
-      <div className="grid md:grid-cols-2 gap-8 max-w-6xl mx-auto text-white text-center">
+    <div
+      className="flex-1 p-4 py-10  w-full backdrop-blur-[1.5px]
+             border-4 rounded-2xl border-blue-50 bg-yellow-050"
+    >
+      <div className="grid md:grid-cols-2 gap-8 max-w-6xl mx-auto text-center">
         {validcourses.map((course, index) => (
           <Link
             key={index}
             href="/room"
             className="flex flex-col items-center justify-center
-             py-6 px-4 
-             transition duration-300 text-black text-center
-             rounded-2xl
-             bg-white-020
-             shadow-xl        
-             backdrop-blur-[1.2px]
-             border-4 border-blue-50
+             py-6 px-4 transition duration-300 text-center rounded-2xl
+             bg-white-020 shadow-xl backdrop-blur-[1.5px] border-4 border-blue-50
+              h-[15rem] group
              "
           >
-            <h3 className="font-bold text-4xl mb-2">{course.name}</h3>
-
-            <h3>{course.professor}</h3>
-            <h3>{"Begins: " + course.beginDate}</h3>
-            <h3>{"Ends: " + course.endDate}</h3>
+            <div className="transition duration-300 group">
+              <h3 className="font-bold text-3xl ">
+                <span className="group-hover:bg-blue-400 group-hover:text-black transition-colors duration-300 ">
+                  {course.name}
+                </span>
+              </h3>
+              <h3>
+                <span className="group-hover:bg-blue-400  group-hover:text-black transition-colors duration-300 ">
+                  {course.professor}
+                </span>
+              </h3>
+              <h3 className="px-4">
+                <span className="group-hover:bg-blue-400 group-hover:text-black transition-colors duration-300 ">
+                  {"Begins: " + course.beginDate}
+                </span>
+              </h3>
+              <h3 className="px-4">
+                <span className="group-hover:bg-blue-400 group-hover:text-black transition-colors duration-300 ">
+                  {"Ends: " + course.endDate}
+                </span>
+              </h3>
+            </div>
           </Link>
         ))}
       </div>
@@ -43,26 +59,44 @@ function renderCourseButtons() {
 
 export default function LandingPage() {
   return (
-    <div className=" overflow-y-auto dot-grid">
+    <div className="overflow-y-auto dot-grid min-h-screen flex flex-col">
       <h1
         className="absolute items-center justify-between shadow-md top-6
-         left-8 right-8 z-[5] bg-white rounded-lg pw-full 
-         text-2xl font-bold text-left py-2 px-4 flex 
-         border-2 border-blue-50"
+     left-8 right-8 z-[5] bg-white rounded-lg 
+     text-2xl font-bold text-left py-2 px-3 flex 
+     border-3 border-blue-50"
       >
-        <span className="text-lg font-bold text-black">AskEasy</span>
+        <span className="text-lg font-bold px-2">AskEasy</span>
 
-        <button className="rounded-lg border border-stone-300 px-4 py-2 text-sm font-medium hover:bg-stone-100 transition">
-          QR Code Login
-        </button>
+        <div className="space-x-3">
+          <button className="px-2 rounded-lg border border-stone-300 px-4 py-2 text-sm font-medium hover:bg-stone-100 transition">
+            Create a Class
+          </button>
+          <button className="rounded-lg border border-stone-300 px-4 py-2 text-sm font-medium hover:bg-stone-100 transition">
+            Qr Code login
+          </button>
+        </div>
       </h1>
 
-      <div className="flex flex-col py-30 min-h-screen ">
-        <div className="h-50 flex flex-col pb-50">
-          <h1 className="top-24 text-4xl py-4 text-center  ">Classrooms</h1>
-          <div className="flex-1 f-jetbrains-mono  px-4 space-y-6">{renderCourseButtons()}</div>
-        </div>
+      <div className="flex-1 pt-32 pb-10">
+        <h1 className="text-4xl py-4 text-center">Classrooms</h1>
+        <div className="flex-1 px-4 py-4">{renderCourseButtons()}</div>
       </div>
+
+      <footer className=" flex text-white bg-red-500 justify-center">
+        <div className=" py-8 flex bg-black grid md:grid-cols-2 gap-8 items-center border-blue-50">
+          <div className="text-left mx-auto ">
+            <h1 className="font-bold py-1 text-xl">Contact Us:</h1>
+            <p>support@askeasy.com</p>
+          </div>
+
+          <div className="text-left mx-auto">
+            <h1 className="font-bold py-1 text-xl">Support:</h1>
+            <p>For Professors</p>
+            <p>For Students</p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
