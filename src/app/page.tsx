97eb0bb07ff76004310a9a7d60a16dd797844a8f
Jaden Scali <jadenrscali@gmail.com>
2026-03-10 14:54:32 -0400

    fix: courses now fill at least the screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,14 @@ const placeholder_user: User = {
 
 export default function LandingPage() {
   return (
-    <div className="max-h-screen flex flex-col dot-grid relative">
+    <div className="min-h-screen flex flex-col dot-grid relative">
       {header(placeholder_user)}
-      <div className="overflow-y-auto">
-        <div className="flex-1 p-5 pt-32 items-center justify-center pb-10">
+      <div className="overflow-y-auto flex-1 flex flex-col">
+        <div className="flex-1 p-5 pt-32 pb-10 flex flex-col items-center">
           <h1 className="text-4xl font-bold py-4 text-center">Classrooms</h1>
-          {placeholder_user.role === "PROFESSOR" ? <ProfCourseViewer /> : <CourseViewer />}
+          <div className="w-full max-w-7xl mx-auto flex-1 flex flex-col">
+            {placeholder_user.role === "PROFESSOR" ? <ProfCourseViewer /> : <CourseViewer />}
+          </div>
         </div>
         {footer()}
       </div>
