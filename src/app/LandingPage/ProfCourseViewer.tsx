83c93802934c73b36dea08855ee0f6dfcca9a474
Jaden Scali <jadenrscali@gmail.com>
2026-03-10 10:44:18 -0400

    manage class -> delete class

--- src/app/LandingPage/ProfCourseViewer.tsx
+++ src/app/LandingPage/ProfCourseViewer.tsx
@@ -52,7 +52,7 @@ export default function ProfCourseButtons() {
                       className="rounded-lg bg-stone-100 hover:bg-stone-300 px-4 py-2 text-md font-medium text-stone-900 transition-colors"
                       onClick={(e) => e.stopPropagation()} // Prevent card click when clicking button
                     >
-                      Manage Class
+                      Delete Class
                     </Link>
                   </div>
                 </div>
