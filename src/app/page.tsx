29af25ff8cdb8e9d2beaa43f13e186e6efb77dcf
Marwan <marwan.yousef@mail.utoronto.ca>
2026-03-13 20:56:01 -0400

    final lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { User } from "@/utils/types";
 import CourseViewer from "./components/CourseViewer";
 import footer from "./components/footer";
