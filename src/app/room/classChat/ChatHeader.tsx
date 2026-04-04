75d6c41ae7658fabe4e03b4c244b6bafc7683f36
Marwan <marwan.yousef@mail.utoronto.ca>
2026-04-04 01:58:07 -0400

    lint

--- src/app/room/classChat/ChatHeader.tsx
+++ src/app/room/classChat/ChatHeader.tsx
@@ -157,7 +157,6 @@ export default function ChatHeader({
                   )}
                 </button>
               )}
-
               <Link
                 href="/"
                 aria-label="Go to main page"
